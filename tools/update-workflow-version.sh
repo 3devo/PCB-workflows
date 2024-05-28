4ce39a576b50a13572e1fd96026fd69fe2e492c0
Matthijs Kooijman <matthijs@stdin.nl>
2024-05-28 12:49:19 +0200

    update-workflow-version.sh: Fix paths with spaces

--- tools/update-workflow-version.sh
+++ tools/update-workflow-version.sh
@@ -2,7 +2,7 @@
 
 # This script updates the workflow file in the parent git repository to
 # match the submodule versios.
-SUBMODULE_ROOT=$(cd $(dirname "$0") && git rev-parse --show-toplevel)
+SUBMODULE_ROOT=$(cd "$(dirname "$0")" && git rev-parse --show-toplevel)
 REPO_ROOT=$(cd "${SUBMODULE_ROOT}" && git rev-parse --show-superproject-working-tree)
 
 # Update SHA in dispatcher
